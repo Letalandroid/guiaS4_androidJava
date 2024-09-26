7b4d7832286df72ca0c44a39b2afd6165830abf4
Ucv <ucv@ucv.edu.pe>
2024-09-26 09:04:27 -0500

    Set values on activity1

--- app/src/main/java/com/ucheve/navegarentreactivities/MainActivity.java
+++ app/src/main/java/com/ucheve/navegarentreactivities/MainActivity.java
@@ -1,14 +1,34 @@
 package com.ucheve.navegarentreactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText txtNombre;
+    Spinner spnActividad;
+    Button btnEnviar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtNombre = findViewById(R.id.txtNombre);
+        spnActividad = findViewById(R.id.spnActividad);
+        btnEnviar = findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(v -> {
+            goToActivity();
+        });
+    }
+
+    private void goToActivity() {
+        Intent intent = new Intent();
     }
 }
